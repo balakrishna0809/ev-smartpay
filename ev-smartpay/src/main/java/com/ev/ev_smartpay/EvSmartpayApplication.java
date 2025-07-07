6525795be77f2504fce978cf84012ce7ca04e8e4
Balakrishna0809 <rbk464.kcp@gmail.com>
2025-07-07 20:09:29 +0530

    Initial commit- EV SmartPay

---
+++ ev-smartpay/src/main/java/com/ev/ev_smartpay/EvSmartpayApplication.java
@@ -0,0 +1,13 @@
+package com.ev.ev_smartpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvSmartpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvSmartpayApplication.class, args);
+	}
+
+}
---
+++ ev-smartpay/src/test/java/com/ev/ev_smartpay/EvSmartpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.ev_smartpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvSmartpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
